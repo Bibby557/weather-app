fcc09a85298709d6e2404043f9cc976df493045e
Bibby557 <bibby557@hotmail.com>
2024-11-05 14:35:57 +0000

    Set default search to display correct data on load

--- src/script.js
+++ src/script.js
@@ -15,9 +15,10 @@ function searchCity(city) {
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#city-input");
-
   searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Nottingham");
